3611ee1cd99ac0c2c83dfaa475b3c2a4bfb8bca3
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2017-10-12 10:02:07 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -21,7 +21,7 @@ def render_response():
         reply = float(request.args['miles'])*1.60934
     elif 'Celcius' in request.args:
         reply = float(request.args['Celcius'])*1.8+32
-    else:
+    else 'lbs' in request.args:
         reply = float(request.args['lbs'])*0.453592
     return render_template('response.html', response = reply)
 if __name__=="__main__":
